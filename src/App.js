c051172ac15450cb67ad4613bd27306f258c59e1
NavyaKoppada <52247480+NavyaKoppada@users.noreply.github.com>
2024-08-05 14:31:03 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Greet from "./components/greet";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet></Greet>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet(){
+//   return (
+//     <h1>Hello Navya.</h1>
+//   )
+// }
+
+const Greet = () => <h1>Hello Navya.</h1>;
+
+export default Greet;
